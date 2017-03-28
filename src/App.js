732141db02a54d153b0b7546d15bafcaec63cda2
Sam Corcos <sam@corcos.io>
2017-03-27 18:46:03 -0700

    create minimum reproducible bug

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const styles = {
+  container: {
+    position: 'relative',
+    height: 100,
+    width: 300,
+  },
+  input: {
+    position: 'absolute',
+    bottom: 30,
+    left: 0,
+    color: 'black',
+    borderTop: 'none',
+    borderLeft: 'none',
+    borderRight: 'none',
+    borderBottom: '1px solid black',
+    borderRadius: 0,
+    marginLeft: 30,
+    marginRight: 30,
+    fontSize: '2.6em',
+    width: 240,
+    textAlign: 'center',
+    backgroundColor: 'transparent',
+    outline: 'none',
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div style={styles.container}>
+          <input
+            style={styles.input}
+            placeholder="PLATE"
+            type="text"  />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
